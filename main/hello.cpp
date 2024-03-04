894495c3c51c69ec5f143466938ff54c7dba8c9d
Sakthe-Balan <sakthebalan2003@gmail.com>
2024-03-04 20:09:29 +0530

    s

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,9 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and un\n";
-  cout <<"Im Sakthe";
+
+  cout <<"Im Sakthe \n";
   return 0;
 }
